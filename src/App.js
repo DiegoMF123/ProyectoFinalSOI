eda53620519b9e010361139b033784e1df7b1074
Diego <diegisseb@gmail.com>
2021-10-05 22:25:36 -0600

    Cambios del 1er commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="Pàgina principal">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Visiòn</h1>
         </header>
         <p className="App-intro">
-          Universidad Mariano Galvez, Curso de SO.I Diego Mijangos
+          Universidad Mariano Galvez, Esta es la visiòn
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <br>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Misiòn</h1>
+        </header>
+        <p className="App-intro">
+          Universidad Mariano Galvez, Esta es la misiòn
+
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <br>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Objetivos</h1>
+        </header>
+        <p className="App-intro">
+          Universidad Mariano Galvez, Estos son los objetivos
 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
