182e00073dfdd7f02d9be756e92f6566c1dd69dd
Diego <diegisseb@gmail.com>
2021-10-05 23:06:50 -0600

    Ultimos Cambios 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './PaginaPrincipal';
+import PaginaPrincipal from './PaginaPrincipal';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
