fd59441871e92f7510668139ebb8b898feb1df89
Diego <diegisseb@gmail.com>
2021-10-05 22:36:11 -0600

    Cambios 2

--- src/App.js
+++ src/App.js
@@ -11,26 +11,9 @@ class App extends Component {
           <h1 className="App-title">Visiòn</h1>
         </header>
         <p className="App-intro">
-          Universidad Mariano Galvez, Esta es la visiòn
+          Seccion para la visiòn
 
-        </p>
-        <br>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Misiòn</h1>
-        </header>
-        <p className="App-intro">
-          Universidad Mariano Galvez, Esta es la misiòn
-
-        </p>
-        <br>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Objetivos</h1>
-        </header>
-        <p className="App-intro">
-          Universidad Mariano Galvez, Estos son los objetivos
-
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
